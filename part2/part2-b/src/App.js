735169087ee76ee29db9148a4d2bbf8ffa4e3cf1
saliulia <iulia.salau16@gmail.com>
2023-08-15 16:19:50 +0300

    the PhoneBook step2

---
+++ part2/part2-b/src/App.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [newName, setNewName] = useState('');
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const addPerson = (event) => {
+    event.preventDefault();
+
+    // Check if the person's name already exists in the phonebook
+    if (persons.some(person => person.name === newName)) {
+      alert(`${newName} is already added to the phonebook.`);
+    } else {
+      const newPerson = { name: newName };
+      setPersons([...persons, newPerson]);
+      setNewName('');
+    }
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person => <li key={person.name}>{person.name}</li>)}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
